16698f42ed0fdbcc769f738cf3650791771b6986
baggioginola <mariocuevas88@outlook.com>
2016-08-03 17:06:33 -0700

    - template release

--- includes/public/js/custom/baseFunctions.js
+++ includes/public/js/custom/baseFunctions.js
@@ -4,39 +4,10 @@
 /* =================
 global root variables
 ===================== */
-$(document).ready(function ()
+jQuery(document).ready(function ()
 {
-    url = BASE_ROOT + 'categorias/getAll';
-
-    $.post(url, function (response, status) {
-        if (status == 'success') {
-            if(response.status == 200) {
-                var subMenuContent = $('.sub-menu-content');
-                var ul = $('<ul/>').appendTo(subMenuContent);
-
-                $.each(response.data, function(key, value)
-                {
-                    var li = $('<li/>').appendTo(ul);
-                    var a = $('<a/>').text(value.nombre).attr('href',BASE_ROOT + value.key_nombre).appendTo(li);
-                });
-            }
-        }
-    }, 'json');
-});
-
-function getImage(root_images, name, i)
-{
-    var url = root_images + name + '-' + i + '.jpg';
-    var exists = $.ajax({
-        url: url,
-        type: "POST",
-        cache: false,
-        dataType: 'json',
-        async: false
+    jQuery("#slider").easySlider({
+        auto: true,
+        continuous: true
     });
-
-    if(exists.status != 200) {
-        return root_images + name + '-' + i + '.png';
-    }
-    return url;
-}
+});
